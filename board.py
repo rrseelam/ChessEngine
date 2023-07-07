1dee95d4b609d314df1c8745e40c886ec036ff2e
Rishith Seelam <rseelam@umich.edu>
2023-07-07 07:42:33 -0400

    bug fixes (many other issues still remain)

--- board.py
+++ board.py
@@ -111,7 +111,11 @@ class Board:
         self.locations[code[0]][code[1]] = change
         return res
 
-    def get_loc(self, loc) -> tuple:
+    def get_loc(self, loc:tuple) -> tuple:
+        code = loc
+        return self.locations[code[0]][code[1]]
+    
+    def get_loc(self, loc:str) -> tuple:
         code = self.to_code(loc)
         return self.locations[code[0]][code[1]]
 
@@ -168,14 +172,14 @@ class Board:
         output = ""
 
         output += f"  A B C D E F G  \n"
-        output += f"8 {self.locations['A8']}{self.locations['B8']}{self.locations['C8']}{self.locations['D8']}{self.locations['E8']}{self.locations['F8']}{self.locations['G8']}{self.locations['H8']} 8\n"
-        output += f"7 {self.locations['A7']}{self.locations['B7']}{self.locations['C7']}{self.locations['D7']}{self.locations['E7']}{self.locations['F7']}{self.locations['G7']}{self.locations['H7']} 7\n"
-        output += f"6 {self.locations['A6']}{self.locations['B6']}{self.locations['C6']}{self.locations['D6']}{self.locations['E6']}{self.locations['F6']}{self.locations['G6']}{self.locations['H6']} 6\n"
-        output += f"5 {self.locations['A5']}{self.locations['B5']}{self.locations['C5']}{self.locations['D5']}{self.locations['E5']}{self.locations['F5']}{self.locations['G5']}{self.locations['H5']} 5\n"
-        output += f"4 {self.locations['A4']}{self.locations['B4']}{self.locations['C4']}{self.locations['D4']}{self.locations['E4']}{self.locations['F4']}{self.locations['G4']}{self.locations['H4']} 4\n"
-        output += f"3 {self.locations['A3']}{self.locations['B3']}{self.locations['C3']}{self.locations['D3']}{self.locations['E3']}{self.locations['F3']}{self.locations['G3']}{self.locations['H3']} 3\n"
-        output += f"2 {self.locations['A2']}{self.locations['B2']}{self.locations['C2']}{self.locations['D2']}{self.locations['E2']}{self.locations['F2']}{self.locations['G2']}{self.locations['H2']} 2\n"
-        output += f"1 {self.locations['A1']}{self.locations['B1']}{self.locations['C1']}{self.locations['D1']}{self.locations['E1']}{self.locations['F1']}{self.locations['G1']}{self.locations['H1']} 2\n"
+        output += f"8 {self.get_loc('A8')[0]}{self.get_loc('B8')[0]}{self.get_loc('C8')[0]}{self.get_loc('D8')[0]}{self.get_loc('E8')[0]}{self.get_loc('F8')[0]}{self.get_loc('G8')[0]}{self.get_loc('H8')[0]} 8\n"
+        output += f"7 {self.get_loc('A7')[0]}{self.get_loc('B7')[0]}{self.get_loc('C7')[0]}{self.get_loc('D7')[0]}{self.get_loc('E7')[0]}{self.get_loc('F7')[0]}{self.get_loc('G7')[0]}{self.get_loc('H7')[0]} 7\n"
+        output += f"6 {self.get_loc('A6')[0]}{self.get_loc('B6')[0]}{self.get_loc('C6')[0]}{self.get_loc('D6')[0]}{self.get_loc('E6')[0]}{self.get_loc('F6')[0]}{self.get_loc('G6')[0]}{self.get_loc('H6')[0]} 6\n"
+        output += f"5 {self.get_loc('A5')[0]}{self.get_loc('B5')[0]}{self.get_loc('C5')[0]}{self.get_loc('D5')[0]}{self.get_loc('E5')[0]}{self.get_loc('F5')[0]}{self.get_loc('G5')[0]}{self.get_loc('H5')[0]} 5\n"
+        output += f"4 {self.get_loc('A4')[0]}{self.get_loc('B4')[0]}{self.get_loc('C4')[0]}{self.get_loc('D4')[0]}{self.get_loc('E4')[0]}{self.get_loc('F4')[0]}{self.get_loc('G4')[0]}{self.get_loc('H4')[0]} 4\n"
+        output += f"3 {self.get_loc('A3')[0]}{self.get_loc('B3')[0]}{self.get_loc('C3')[0]}{self.get_loc('D3')[0]}{self.get_loc('E3')[0]}{self.get_loc('F3')[0]}{self.get_loc('G3')[0]}{self.get_loc('H3')[0]} 3\n"
+        output += f"2 {self.get_loc('A2')[0]}{self.get_loc('B2')[0]}{self.get_loc('C2')[0]}{self.get_loc('D2')[0]}{self.get_loc('E2')[0]}{self.get_loc('F2')[0]}{self.get_loc('G2')[0]}{self.get_loc('H2')[0]} 2\n"
+        output += f"1 {self.get_loc('A1')[0]}{self.get_loc('B1')[0]}{self.get_loc('C1')[0]}{self.get_loc('D1')[0]}{self.get_loc('E1')[0]}{self.get_loc('F1')[0]}{self.get_loc('G1')[0]}{self.get_loc('H1')[0]} 2\n"
         output += f"  A B C D E F G  \n"
 
         print(output)
@@ -187,7 +191,7 @@ class Board:
         for (rank, file) in pos:
             if not self.validposition((rank, file)):
                 continue
-            if self.board[rank][file][0] != ' ' and self.board[rank][file][1] == color:
+            if self.locations[rank][file][0] != ' ' and self.locations[rank][file][1] == color:
                 continue
             res.append((rank, file))
 
@@ -202,13 +206,13 @@ class Board:
         right = 1
         while (self.validposition((r + right, f + up))):
 
-            if self.board[r + up][f + right][0] == ' ':
+            if self.locations[r+up][f+right] == ' ':
                 res.append((r + up, f + right))
                 up += 1
                 right += 1
                 continue
 
-            if self.board[r + up][f + right][1] != color:
+            if self.get_loc[r + up][f + right][1] != color:
                 res.append((r + up, f + right))
 
             break
@@ -218,13 +222,13 @@ class Board:
         left = -1
         while (self.validposition((r + left, f + up))):
 
-            if self.board[r + up][f + left][0] == ' ':
+            if self.locations[r + up][f + left][0] == ' ':
                 res.append((r + up, f + left))
                 up += 1
                 left -= 1
                 continue
 
-            if self.board[r + up][f + left][1] != color:
+            if self.locations[r + up][f + left][1] != color:
                 res.append((r + up, f + left))
 
             break
@@ -234,13 +238,13 @@ class Board:
         right = 1
         while (self.validposition((r + right, f + down))):
 
-            if self.board[r + down][f + right][0] == ' ':
+            if self.locations[r + down][f + right][0] == ' ':
                 res.append((r + down, f + right))
                 down -= 1
                 right += 1
                 continue
 
-            if self.board[r + down][f + right][1] != color:
+            if self.locations[r + down][f + right][1] != color:
                 res.append((r + down, f + right))
 
             break
@@ -250,13 +254,13 @@ class Board:
         left = -1
         while (self.validposition((r + left, f + down))):
 
-            if self.board[r + down][f + left][0] == ' ':
+            if self.locations[r + down][f + left][0] == ' ':
                 res.append((r + down, f + left))
                 down -= 1
                 left -= 1
                 continue
 
-            if self.board[r + down][f + left][1] != color:
+            if self.locations[r + down][f + left][1] != color:
                 res.append((r + down, f + left))
 
             break
@@ -271,12 +275,12 @@ class Board:
         right = 1
         while (self.validposition(r + right, f)):
 
-            if self.board[r + right][f][0] == ' ':
+            if self.locations[r + right][f][0] == ' ':
                 res.append((r + right, f))
                 right += 1
                 continue
 
-            if self.board[r + right][f][1] != color:
+            if self.locations[r + right][f][1] != color:
                 res.append((r + right, f))
 
             break
@@ -285,12 +289,12 @@ class Board:
         left = -1
         while (self.validposition(r + left, f)):
 
-            if self.board[r + left][f][0] == ' ':
+            if self.locations[r + left][f][0] == ' ':
                 res.append((r + left, f))
                 left -= 1
                 continue
 
-            if self.board[r + left][f][1] != color:
+            if self.locations[r + left][f][1] != color:
                 res.append((r + left, f))
 
             break
@@ -299,12 +303,12 @@ class Board:
         up = 1
         while (self.validposition(r, f + up)):
 
-            if self.board[r][f + up][0] == ' ':
+            if self.locations[r][f + up][0] == ' ':
                 res.append((r, f + up))
                 up += 1
                 continue
 
-            if self.board[r][f + up][1] != color:
+            if self.locations[r][f + up][1] != color:
                 res.append((r, f + up))
 
             break
@@ -313,12 +317,12 @@ class Board:
         down = -1
         while (self.validposition(r, f + down)):
 
-            if self.board[r][f + down][0] == ' ':
+            if self.locations[r][f + down][0] == ' ':
                 res.append((r, f + down))
                 down -= 1
                 continue
 
-            if self.board[r][f + down][1] != color:
+            if self.locations[r][f + down][1] != color:
                 res.append((r, f + down))
 
             break
@@ -408,7 +412,7 @@ class Board:
 if __name__ == '__main__':
     sample = Board()
     sample.print_board()
-    # print("Rook",   sample.get_moves(0, 0))
+    # print("Rook",   sample.get_moves('A!'))
     # print("Knight", sample.get_moves(0, 1))
     # print("Bishop", sample.get_moves(0, 2))
     # print("Queen",  sample.get_moves(0, 3))
